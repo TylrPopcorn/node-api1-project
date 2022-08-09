64fb07a2b3fe8e2d0f21423c0a0209e20d0a85fc
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-08-08 20:46:05 -0400

    finished

--- api/server.js
+++ api/server.js
@@ -1,3 +1,112 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const User = require("./users/model")
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+server.use(express.json())
+
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist."
+            })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: "Please provide name and bio for the user"
+                })
+            } else {
+                const updatedUser = await User.update(req.params.id, req.body)
+                res.status(200).json(updatedUser)
+            }
+        }
+
+    } catch (err) {
+        res.status(500).json({
+            message: "error updating user",
+            err: err.message,
+        })
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const UserMaybe = await User.findById(req.params.id)
+        //console.log(userMaybe)
+        if (!UserMaybe) {
+            res.status(404).json({
+                message: "The user with the speccified ID does not exist"
+            })
+        } else {
+            const deleteU = await User.remove(UserMaybe.id)
+            res.status(200).json(deleteU)
+        }
+    } catch (err) {
+        res.status(505).json({
+            message: 'error creating user',
+            err: err.message,
+        })
+    }
+})
+
+server.post("/api/users", (req, res) => {
+    const user = req.body;
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user'
+        })
+    } else {
+        User.insert(user)
+            .then(val => {
+                // console.log(val)
+                res.status(201).json(val)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "error creating user",
+                    err: err.message,
+                })
+            })
+    }
+})
+
+server.get("/api/users", (req, res) => {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+})
+
+server.get("/api/users/:id", (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+            })
+        })
+})
+
+server.use("*", (req, res) => {
+    res.status(404).json({
+        message: "Not found."
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+//DEPENDENCIES/NEEDS:
+/*
+    npm i -D nodemon
+    npm install express
+*/
+
 const server = require('./api/server');
 
 const port = 9000;
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log("Hey there")
+})
+
